c353dfbd8852dc641c4a6dd2fa1d5604b8b2902b
KenanTaylor <kenan.taylor99@gmail.com>
2023-08-13 17:55:35 -0400

    conection

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,30 @@
+import React,{ useState, useEffect } from 'React';
 import './App.css';
+import Axios from 'axios'
 
 function App() {
+
+  const {movieName, setMovieName} = useState('');
+  const {review, setReview} = useState('');
+
+  const submitReview = () =>{
+    Axios.post("http://localhost:3000/");
+  };
+
   return (
     <div className="App"> <h1>Movie Feed Back</h1>
 
        <div className='form'>
         <label>Movie Name</label>
-        <input type='text' name='Moviename'/>
+        <input type='text' name='Moviename' onChange={(e)=>{
+               setMovieName(e.target.value);
+        }}/>
         <label>Review</label>
-        <input type='text' name='Review'/>
+        <input type='text' name='Review'onChange={(e)=>{
+               setReview(e.target.value);
+        }}/>
 
-        <button>Suubmit</button>
+        <button onClick={submitReview}>Submit</button>
        </div>
   </div>
   );
